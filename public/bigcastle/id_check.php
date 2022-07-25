6ba93fbe166389586bbe61a2768a7960011ea075
madforre <madforre@gmail.com>
2022-07-25 11:13:17 +0900

    docs(review): codes

--- public/bigcastle/id_check.php
+++ public/bigcastle/id_check.php
@@ -3,7 +3,7 @@
 require_once('./db/db.php');
 
 if($_POST['user_id'] != NULL){
-    $id_check = mq("select * from tb_user where user_id='{$_POST['USER_ID']}'");
+    $id_check = mq("select * from tb_user where user_id='{$_POST['USER_ID']}'"); // mq 함수가 정의되지 않음
     $id_check = $id_check->fetch_array();
 
     if($id_check >= 1){
@@ -14,4 +14,5 @@ if($_POST['user_id'] != NULL){
     }
 }
 
+// 하단에는 닫힘 꺽쇠를 사용하지 않음 - psr 준수
 ?>
