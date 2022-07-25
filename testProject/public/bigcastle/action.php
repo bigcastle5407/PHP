d27cd00231261b54ac59f335e489f91705433194
bigcastle <bigcastle5407@naver.com>
2022-07-25 10:58:09 +0900

    fix: 로그인 기능 수정, 같은 데이터 DB추가 수정

--- testProject/public/bigcastle/action.php
+++ testProject/public/bigcastle/action.php
@@ -13,42 +13,90 @@
                 $user_date = $_POST['user_date'];
                 $user_tel = $_POST['user_tel'];
                 
+                if(!is_null($user_id)){
+                    
+                    $conn = mysqli_connect('localhost','root','qwe123','testdb');
+                    
+                    $sql = "SELECT user_id FROM tb_user WHERE user_id = '$user_id';";
 
-                // echo "$user_name.','.$user_id.','.$user_pw.','.$user_gender.','.$user_date.','$user_tel'";
-                
-                $sql = $db -> prepare("INSERT INTO tb_user VALUE('$user_name', '$user_id','$user_pw', '$user_gender','$user_date','$user_tel')");
-              
+                    $result = mysqli_query($conn, $sql);
 
+                    while($row = mysqli_fetch_array($result)){
+                        $user_id_e = $row['user_id'];
+                        echo("<script>alert('이미 사용중인 아이디입니다.'); history.back();</script>");
+                    }
 
-                if($user_pw != $user_pw2){
-                    echo("<script>alert('비밀번호가 일치하지 않습니다.'); history.back();</script>");
+                    if($user_id == $user_id_e){
+                        $wu = 1;
+                    }else if($user_pw != $user_pw2){
+                        echo("<script>alert('비밀번호가 일치하지 않습니다.'); history.back();</script>");
+                        $wp = 1;
+                    }else{
+                        // $encrypted_password = password_hash($password, PASSWORD_DEFAULT);
+                        $sql_add = "INSERT INTO tb_user VALUES ('$user_name', '$user_id','$user_pw', '$user_gender','$user_date','$user_tel');";
+                    }
+                    mysqli_query($conn, $sql_add);
+                    header('Location: bigcastle/main.php');
+
+                }else{
+                    echo "회원가입 실패";
                 }
-
-                $sql -> execute();
-                echo "<script> Location.replace('/bigcastle/main.php')</script>";
-
+                
                 break;
+                
+                
+                
+                
+                // if($user_pw != $user_pw2){
+                //     echo("<script>alert('비밀번호가 일치하지 않습니다.'); history.back();</script>");
+                // }
+
+                // $sql = $db -> prepare("INSERT INTO tb_user VALUE('$user_name', '$user_id','$user_pw', '$user_gender','$user_date','$user_tel')");
+                
+                // $sql -> execute();
+                // echo "<script> Location.replace('/bigcastle/main.php')</script>";
+
             
 
             case 'login':
                 $user_id = $_POST['user_id'];
                 $user_pw = $_POST['user_pw'];
-              
         
-                $sql = $db -> prepare("SELECT * FROM tb_user WHERE user_id=$user_id");
-                $sql -> execute();
+
+                $conn = mysqli_connect('localhost','root','qwe123','testdb');
+                    
+                $sql = "SELECT * FROM tb_user WHERE user_id='{$user_id}' AND user_pw = '{$user_pw}'";
+
+                $result = mysqli_query($conn, $sql);
+                
+                $row = $result -> fetch_array(MYSQLI_ASSOC);
+
+
+
+
+                if($row != null){
+                    $_SESSION['user_id'] = $user_id;
+                    $_SESSION['user_id'] = $row['user_id'];
+                    echo $_SESSION['user_id'].'님 안녕하세요';
+                    header('Location: /bigcastle/main.php');
+
                
+                }
+
+                if($row == null){
+                    echo '로그인 실패';
+                }
+                
+            
 
                 if(!$user_id){
                     echo("<script>alert('아이디를 입력해주세요'); history.back();</script>");
                 
                 }
-                $_SESSION['user_id'] = $user_id;
-               
+                
 
                 // echo "<script> Location.replace('/bigcastle/main.php')</script>";
 
-                header('Location: /bigcastle/main.php');
 
                 break;
 
@@ -65,7 +113,7 @@
                 $user_id = $_POST['user_id'];
                 
                 if($_POST['user_id'] != NULL){
-                    $id_check = mq("select * from tb_user where user_id='{$user_id}'");
+                    $id_check = mq("select * from tb_user where user_id='" .$user_id."'");
                     $id_check = $id_check->fetch_array();
                     
                     if($id_check >= 1){
--- testProject/public/bigcastle/register.php
+++ testProject/public/bigcastle/register.php
@@ -1,6 +1,8 @@
         <?php
         require_once('./db/db.php');
         ?>
+
+
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -9,91 +11,88 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Document</title>
             <script type="text/javascript" src="../js/jquery-3.2.1.js"></script>
-            <script>
-            $(document).ready(function(e) { 
-	        $(".check").on("keyup", function(){ //check라는 클래스에 입력을 감지
-		    var self = $(this); 
-		    var user_id; 
-		
-		    if(self.attr("user_id") === "user_id"){ 
-			user_id = self.val(); 
-		    } 
-		
-		    $.post( //post방식으로 id_check.php에 입력한 userid값을 넘깁니다
-			"action.php?mode=id_check",
-			{ user_id : user_id }, 
-			function(data){ 
-				if(data){ //만약 data값이 전송되면
-					self.parent().parent().find("div").html(data); //div태그를 찾아 html방식으로 data를 뿌려줍니다.
-					self.parent().parent().find("div").css("color", "#F00"); //div 태그를 찾아 css효과로 빨간색을 설정합니다
-				}
-			}
-		);
-	});
-});
-</script>
-            
-        </head>
-        <body>
-            <form action='action.php?mode=register' method='post'>
-                <h1>회원가입 페이지</h1>
-                <fieldset>
-                    <legend>입력사항</legend>
-                    <table>
-                        <tr>
-                            <td>이름</td>
-                            <td><input type ='text' id='user_name' name = 'user_name' placeholder="이름을 입력해주세요"></td>
-                           
-                        </tr>
-                        <tr>
-                            <td>아이디</td>
-                            <td><input type ='text' id='user_id' name = 'user_id' class = "check" placeholder="아이디를 입력해주세요"></td>
-                            <td><div id = "id_check">아이디가 실시간으로 검색됩니다</div></td>
-                        </tr>
-                        <tr>
-                            <td>비밀번호</td>
-                            <td><input type ='password' id='user_pw' name = 'user_pw' placeholder="비밀번호를 입력해주세요"></td>
-                            
-                        </tr>
-                        <tr>
-                            <td>비밀번호 확인</td>
-                            <td> <input type ='password' id ='user_pw2' name = 'user_pw2' placeholder="비밀번호를 다시 입력해주세요"></td>
-                            
-                        </tr>
-                        <tr>
-                            <td>성별</td>
-                            <td>
-                                 <input type ='radio' id = 'user_gender' name = 'user_gender' value='m' checked>남
-                                <input type ='radio' id = 'user_gender' name = 'user_gender' value='w'>여
-                            </td>
-                            
-                        </tr>
-                        <tr>
-                            <td>생년월일</td>
-                            <td><input type="date" id = "user_date" name ="user_date"></td>
-                           
-                        </tr>
-                        <tr>
-                            <td>전화번호</td>
-                            <td><input type="text" maxlength = "13" name = "user_tel" id="tel1" placeholder="핸드폰번호를 입력해주세요"></td>
-                            
-                        </tr>
-                   
-                    
-                    <br><br><br>
-                    <tr>
-                        <td>
-                            <input type ='submit' value='가입'> &nbsp;
-                            <input type ='button' value='뒤로가기' onClick='history.back()'>
-                        </td>
-                    </tr>
-                    </table>
-                </fieldset>
-            </form>
+           
 
-            <script>
-    	function autoHypenPhone(str){
-            str = str.replace(/[^0-9]/g, '');
+    <script>
+        function checkId() {
+            window.open("checkId.php?user_id=" + document.register.user_id.value,"IDcheck","left=100, top=100, width=300, height=300, scrollbars=no, resizeable=no");
+        }
+    </script>
+
+    <script>
+            function joinform_check(){
+                
+                var user_name = document.getElementById('user_name');
+                var user_id = document.getElementById('user_id');
+                var user_pw = document.getElementById('user_pw');
+                var user_pw2 = document.getElementById('user_pw2');
+                var user_gender = document.getElementById('user_gender');
+                var user_date = document.getElementById('user_date');
+                var user_tel = document.getElementById('user_tel');
+                
+                if(user_name.value == ""){
+                    alert("이름을 입력하세요");
+                    user_id.focus();
+                    return false;
+                };
+
+                // var id_ck = /^(?=,*[a-zA-Z])(?=,*[0-9]),{1,8}$/;
+
+                // if(!id_ck.test(user_id.value)){
+                //     alert("아이디를 문자+숫자 조합으로 입력해주세요");
+                //     user_id.focus();
+                //     return false;
+                // };
+
+                if(user_id.value == ""){
+                    alert("아이디를 입력하세요");
+                    user_id.focus();
+                    return false;
+                };
+
+                // var password_ck = /^(?=,*[a-zA-Z])(?=,*[!@#$%^*+=-])(?=,*[0-9]),{1,8}$/;
+
+                // if(!password_ck.test(user_pw.value)){
+                //     alert("비밀번호는 영문자+숫자+특수문자 조합으로 8자리를 입력해주세요");
+                //     user_pw.focus();
+                //     return false;
+                // };
+
+                if(user_pw.value == ""){
+                    alert("비밀번호를 입력하세요");
+                    user_pw.focus();
+                    return false;
+                };
+
+                if(user_pw.value !== user_pw2.value){
+                    alert("비밀번호가 일치하지 않습니다.");
+                    user_pw2.focus();
+                    return false;
+                };
+
+                if(user_date.value == ""){
+                    alert("생년월일을 입력하세요");
+                    user_date.focus();
+                    return false;
+                }
+
+                // if(!user_tel.value){
+                //     alert("전화번호를 입력하세요");
+                //     user_tel.focus();
+                //     return false;
+                // }
+                
+                
+                document.register.submit();
+
+
+            
+            }
+        </script>
+
+        <script>
+    	    function autoHypenPhone(str){
+                str = str.replace(/[^0-9]/g, '');
             var tmp = '';
             if( str.length < 4){
                 return str;
@@ -126,6 +125,71 @@
                     var _val = this.value.trim();
                     this.value = autoHypenPhone(_val) ;
             }
-    	</script>
+
+            </script>
+
+           
+            
+        </head>
+        <body>
+            <form name = "register" action='action.php?mode=register' method='post'>
+                <h1>회원가입 페이지</h1>
+                <fieldset>
+                    <legend>입력사항</legend>
+                    <table>
+                        <tr>
+                            <td>이름</td>
+                            <td><input type ='text' id='user_name' name = 'user_name' placeholder="이름을 입력해주세요"></td>
+                           
+                        </tr>
+                        <tr>
+                            <td>아이디</td>
+                            <td><input type ='text' id='user_id' name = 'user_id' class = "check" placeholder="아이디를 입력해주세요"></td>
+                            <!-- <td><div id = "id_check">아이디가 실시간으로 검색됩니다</div></td> -->
+                            <td><input type="button" value="중복확인" onclick="checkId()"></td>
+                        </tr>
+                        <tr>
+                            <td>비밀번호</td>
+                            <td><input type ='password' id='user_pw' name = 'user_pw' placeholder="비밀번호를 입력해주세요"></td>
+                            
+                        </tr>
+                        <tr>
+                            <td>비밀번호 확인</td>
+                            <td> <input type ='password' id ='user_pw2' name = 'user_pw2' placeholder="비밀번호를 다시 입력해주세요"></td>
+                            
+                        </tr>
+                        <tr>
+                            <td>성별</td>
+                            <td>
+                                 <input type ='radio' id = 'user_gender' name = 'user_gender' value='m' checked>남
+                                <input type ='radio' id = 'user_gender' name = 'user_gender' value='w'>여
+                            </td>
+                            
+                        </tr>
+                        <tr>
+                            <td>생년월일</td>
+                            <td><input type="date" id = "user_date" name ="user_date"></td>
+                           
+                        </tr>
+                        <tr>
+                            <td>전화번호</td>
+                            <td><input type="text" maxlength = "13" name = "user_tel" id="tel1" placeholder="핸드폰번호를 입력해주세요"></td>
+                            
+                        </tr>
+                   
+                    
+                    <br><br><br>
+                    
+                    <tr>
+                        <td>
+                            <button type='button' onclick="joinform_check()";>가입하기 </button> &nbsp;
+                            <input type ='button' value='뒤로가기' onClick='history.back()'>
+                        </td>
+                    </tr>
+                    </table>
+                </fieldset>
+            </form>
+
+           
         </body>
         </html>
