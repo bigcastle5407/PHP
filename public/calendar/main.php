86e55062c8b7ce53a444dbceb1427cba05429e52
bigcastle <bigcastle5407@naver.com>
2022-08-02 09:38:27 +0900

    캘린더 완성

--- public/calendar/main.php
+++ public/calendar/main.php
@@ -1,10 +1,10 @@
 <?php
-//---- 오늘 날짜
-$thisyear = date('Y'); // 4자리 연도
-$thismonth = date('n'); // 0을 포함하지 않는 월
-$today = date('j'); // 0을 포함하지 않는 일
+//오늘 날짜
+$thisyear = date('Y');
+$thismonth = date('m');
+$today = date('j');
 
-//------ $year, $month 값이 없으면 현재 날짜
+//$year, $month 값이 없으면 현재 날짜
 $year = isset($_GET['year']) ? $_GET['year'] : $thisyear;
 $month = isset($_GET['month']) ? $_GET['month'] : $thismonth;
 $day = isset($_GET['day']) ? $_GET['day'] : $today;
@@ -26,17 +26,16 @@ $nextyear = $year + 1;
 $predate = date("Y-m-d", mktime(0, 0, 0, $month - 1, 1, $year));
 $nextdate = date("Y-m-d", mktime(0, 0, 0, $month + 1, 1, $year));
 
-// 1. 총일수 구하기
-$max_day = date('t', mktime(0, 0, 0, $month, 1, $year)); // 해당월의 마지막 날짜
-//echo '총요일수'.$max_day.'<br />';
+//총일수 구하기
+$max_day = date('t', mktime(0, 0, 0, $month, 1, $year));
 
-// 2. 시작요일 구하기
-$start_week = date("w", mktime(0, 0, 0, $month, 1, $year)); // 일요일 0, 토요일 6
+//시작요일 구하기
+$start_week = date("w", mktime(0, 0, 0, $month, 1, $year));
 
-// 3. 총 몇 주인지 구하기
+//총 몇 주인지 구하기
 $total_week = ceil(($max_day + $start_week) / 7);
 
-// 4. 마지막 요일 구하기
+//마지막 요일 구하기
 $last_week = date('w', mktime(0, 0, 0, $month, $max_day, $year));
 ?>
 
@@ -52,32 +51,20 @@ $last_week = date('w', mktime(0, 0, 0, $month, $max_day, $year));
     font.blue {font-family: tahoma;font-size: 20px;color: #0000FF;}
     font.black {font-family: tahoma;font-size: 20px;color: #000000;}
   </style>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="container">
+<div class="container" style="margin-top:60px;">
 <table class="table table-bordered table-responsive">
-  <tr style="text-align:center" >
-    <td>
-        <a href=<?php echo 'main.php?year='.$preyear.'&month='.$month . '&day=1'; ?>>◀◀</a>
-    </td>
-    <td>
-        <a href=<?php echo 'main.php?year='.$pre_year.'&month='.$pre_month . '&day=1'; ?>>◀</a>
-    </td>
-    <td height="50" background="#FFFFFF" colspan="3">
-        <a href=<?php echo 'main.php?year=' . $thisyear . '&month=' . $thismonth . '&day=1'; ?>>
-        <?php echo "&nbsp;&nbsp;" . $year . '년 ' . $month . '월 ' . "&nbsp;&nbsp;"; ?></a>
-    </td>
-    <td>
-        <a href=<?php echo 'main.php?year='.$next_year.'&month='.$next_month.'&day=1'; ?>>▶</a>
-    </td>
-    <td>
-        <a href=<?php echo 'main.php?year='.$nextyear.'&month='.$month.'&day=1'; ?>>▶▶</a>
-    </td>
-  </tr>
-  <tr class="info">
-    <th hight="30">일</td>
+  
+    <div style="text-align:center;font-size:large;">
+        <a href=<?='main.php?year='.$pre_year.'&month='.$pre_month . '&day=1'; ?>><<</a>
+        <a href=<?='main.php?year=' . $thisyear . '&month=' . $thismonth . '&day=1'; ?>>
+        <?="&nbsp;&nbsp;" . $year . '년 ' . $month . '월 ' . "&nbsp;&nbsp;"; ?></a>
+        <a href=<?='main.php?year='.$next_year.'&month='.$next_month.'&day=1'; ?>>>></a>
+    </div>
+  
+  <tr>
+    <th>일</td>
     <th>월</th>
     <th>화</th>
     <th>수</th>
@@ -87,33 +74,28 @@ $last_week = date('w', mktime(0, 0, 0, $month, $max_day, $year));
   </tr>
 
   <?php
-    // 5. 화면에 표시할 화면의 초기값을 1로 설정
+    //매월 1일부터 시작이니까 1로 초기화
     $day=1;
 
-    // 6. 총 주 수에 맞춰서 세로줄 만들기
-    for($i=1; $i <= $total_week; $i++){?>
+    //총 week 수에 맞춰 세로칸 만들기
+    for($i=1; $i <= $total_week; $i++){
+    ?>
   <tr>
     <?php
-    // 7. 총 가로칸 만들기
+    //총 가로칸 만들기
     for ($j = 0; $j < 7; $j++) {
-        // 8. 첫번째 주이고 시작요일보다 $j가 작거나 마지막주이고 $j가 마지막 요일보다 크면 표시하지 않음
         echo '<td height="50" valign="top">';
         if (!(($i == 1 && $j < $start_week) || ($i == $total_week && $j > $last_week))) {
 
             if ($j == 0) {
-                // 9. $j가 0이면 일요일이므로 빨간색
                 $style = "holy";
             } else if ($j == 6) {
-                // 10. $j가 0이면 토요일이므로 파란색
                 $style = "blue";
             } else {
-                // 11. 그외는 평일이므로 검정색
                 $style = "black";
             }
 
-            // 12. 오늘 날짜면 굵은 글씨
             if ($year == $thisyear && $month == $thismonth && $day == date("j")) {
-                // 13. 날짜 출력
                 echo '<font class='.$style.'>';
                 echo $day;
                 echo '</font>';
@@ -122,7 +104,7 @@ $last_week = date('w', mktime(0, 0, 0, $month, $max_day, $year));
                 echo $day;
                 echo '</font>';
             }
-            // 14. 날짜 증가
+            //날짜 증가
             $day++;
         }
         echo '</td>';
